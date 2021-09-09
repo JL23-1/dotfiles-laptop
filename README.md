b6c037289a0cc49d3eae940643433429d4a7c7f9
JL23-1 <71352937+JL23-1@users.noreply.github.com>
2021-09-09 16:25:24 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# dotfiles
+My poggers dotfiles
+WARNING!!!! INSTALL SCRIPT WILL OVERWRITE EXISTING FILES
+GTK theme: https://github.com/vinceliuice/Lavender-theme
---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+read -p "Are you sure? Will replace existing configs [Y/N] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+# i3
+#rm  -f ~/.config/i3/config
+#mkdir -p ~/.config/i3
+#mv ${BASEDIR}/i3 ~/.config/i3/config
+
+
+# bash
+rm  -f ~/.bashrc
+mv ${BASEDIR}/bashrc ~/.bashrc
+
+# i3status
+#rm  -f ~/.config/i3status/config
+#mkdir -p ~/.config/i3status
+#mv ${BASEDIR}/i3status ~/.config/i3status/config
+
+# alacritty
+#rm  -f ~/.config/alacritty/alacritty.yml
+#mkdir -p ~/.config/alacritty
+#mv ${BASEDIR}/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+# xinitrc
+rm  -f ~/.xinitrc
+mv ${BASEDIR}/xinitrc ~/.xinitrc
+
+# lightcord theme
+#rm -f ~/.config/Lightcord_BD/themes/DiscordPlus.theme.css
+#mv ${BASEDIR}/DiscordPlus.theme.css ~/.config/Lightcord_BD/themes/DiscordPlus.theme.css
+fi
+
