4f3cb77fd9bb1572f963272cec077628c72f5e61
JL23-1 <71352937+JL23-1@users.noreply.github.com>
2021-10-28 12:44:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,27 +6,22 @@ echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 # i3
-#rm  -f ~/.config/i3/config
-#mkdir -p ~/.config/i3
-#mv ${BASEDIR}/i3 ~/.config/i3/config
-
-
-# bash
-rm  -f ~/.bashrc
-mv ${BASEDIR}/bashrc ~/.bashrc
+rm  -f ~/.config/i3/config
+mkdir -p ~/.config/i3
+mv ${BASEDIR}/i3 ~/.config/i3/config
 
 # i3status
-#rm  -f ~/.config/i3status/config
-#mkdir -p ~/.config/i3status
-#mv ${BASEDIR}/i3status ~/.config/i3status/config
+rm  -f ~/.config/i3status/config
+mkdir -p ~/.config/i3status
+mv ${BASEDIR}/i3status ~/.config/i3status/config
 
 # alacritty
-#rm  -f ~/.config/alacritty/alacritty.yml
-#mkdir -p ~/.config/alacritty
-#mv ${BASEDIR}/alacritty.yml ~/.config/alacritty/alacritty.yml
+rm  -f ~/.config/alacritty/alacritty.yml
+mkdir -p ~/.config/alacritty
+mv ${BASEDIR}/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 # lightcord theme
-#rm -f ~/.config/Lightcord_BD/themes/DiscordPlus.theme.css
-#mv ${BASEDIR}/DiscordPlus.theme.css ~/.config/Lightcord_BD/themes/DiscordPlus.theme.css
+rm -f ~/.config/Lightcord_BD/themes/DiscordPlus.theme.css
+mv ${BASEDIR}/DiscordPlus.theme.css ~/.config/Lightcord_BD/themes/DiscordPlus.theme.css
 fi
 
